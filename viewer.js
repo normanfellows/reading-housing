fed7f9330c87602443419e0bc151bcd594905db1
Norman Fellows <norman.fellows@orange.fr>
2026-08-09 12:16:58 +0200

    Add files via upload
    Remaining files

---
+++ viewer.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Reading Housing — A Proposition in Formation");
+
+    Mirador.viewer({
+        id: "mirador-viewer",
+        windows: [
+            {
+                manifestId: "manifest.json"
+            }
+        ]
+    });
+
+});
